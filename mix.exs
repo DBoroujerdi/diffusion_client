850dc1092210c3003d9519bc0d1ab441ef96351c
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2017-02-25 11:37:54 +0000

    depend on hash commits for deps

--- mix.exs
+++ mix.exs
@@ -21,14 +21,14 @@ defmodule DiffusionClient.Mixfile do
 
   defp deps do
     [
-      {:gun, git: "https://github.com/ninenines/gun.git"},
-      {:gproc, git: "https://github.com/uwiger/gproc.git"},
+      {:gun, git: "https://github.com/ninenines/gun.git", tag: "bc733a2ca5f7d07f997ad6edf184f775b23434aa"},
+      {:gproc, git: "https://github.com/uwiger/gproc.git", tag: "01c8fbfdd5e4701e8e4b57b0c8279872f9574b0b"},
       {:uuid, "~> 1.1"},
       {:monad, "~> 1.0"},
       {:dialyxir, "~> 0.4", only: [:dev], runtime: false},
       {:ex_spec, "~> 2.0", only: :test},
       {:mock, "~> 0.2.0", only: :test},
-      {:cowboy, git: "https://github.com/ninenines/cowboy.git"},
+      {:cowboy, git: "https://github.com/ninenines/cowboy.git", only: :test, tag: "a45813c60f0f983a24ea29d491b37f0590fdd087"},
       {:ranch, git: "https://github.com/ninenines/ranch.git", override: true}
     ]
   end
